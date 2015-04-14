391b4f309797098d7dca7ede5c8a02ebfadf8609
Bill Koch <koch.149@osu.edu>
2015-04-14 07:04:22 -0400

    Bare bones oauth2 sample

---
+++ src/main/java/org/test/config/OAuth2Config.java
@@ -0,0 +1,34 @@
+package org.test.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(new JdbcTokenStore(dataSource))
+            .authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
+}
---
+++ src/main/resources/db/migration/V2__populate_client_details.sql
@@ -0,0 +1,2 @@
+insert into oauth_client_details(client_id,client_secret,scope,access_token_validity,authorized_grant_types)
+values('client', 'secret','read,write',3600,'password');
